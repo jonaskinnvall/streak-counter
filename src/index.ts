93ff59de7fda13050af57aa33bf935e036f0b34a
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2022-10-28 23:29:22 +0200

    fix(index): update streakCounter
    Return correct object with correct types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export function streakCounter(storage: Storage, date: Date) {
-	return {}
+interface Streak {
+	currentCount: number
+	lastLoginDate: string
+	startDate: string
+}
+
+export function streakCounter(storage: Storage, date: Date): Streak {
+	return {
+		currentCount: 0,
+		lastLoginDate: '28/10/2022',
+		startDate: '28/10/2022',
+	}
 }
