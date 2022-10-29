07e247f1826c361828dbad5ea644724065b5dcfe
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2022-10-29 18:34:56 +0200

    test: add same-day login test
    Streak should not reset on same-day login

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -119,5 +119,15 @@ describe('streakCounter', () => {
 
 			expect(streak.currentCount).toBe(1)
 		})
+
+		it('should not reset streak for same-day login', () => {
+			const date = new Date('10/21/2022')
+			streakCounter(mockLocalStorage, date)
+
+			const dateUpdated = new Date('10/21/2022')
+			const streakUpdated = streakCounter(mockLocalStorage, dateUpdated)
+
+			expect(streakUpdated.currentCount).toBe(2)
+		})
 	})
 })
