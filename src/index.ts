5290fea0128cee64111033153176aa5bb025b4f2
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2022-10-29 11:34:39 +0200

    fix: increment streak if it should

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,20 @@ export function streakCounter(storage: Storage, date: Date): Streak {
 	const streakInLocalStorage = storage.getItem(KEY)
 	if (streakInLocalStorage) {
 		try {
-			const streak = JSON.parse(streakInLocalStorage || '')
+			const streak = JSON.parse(streakInLocalStorage)
+			const state = 'increment'
+			const SHOULD_INCREMENT = state === 'increment'
+
+			if (SHOULD_INCREMENT) {
+				const updatedStreak = {
+					...streak,
+					currentCount: streak.currentCount + 1,
+					lastLoginDate: formattedDate(date),
+				}
+				console.log(updatedStreak)
+
+				return updatedStreak
+			}
 			return streak
 		} catch (error) {
 			console.error('Failed to parse streak from localStorage')
